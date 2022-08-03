aceb9b74b428ba900a251f8e1dcbb4a9ea979435
Travis Jorge <travis.jorge@tylertech.com>
2022-08-03 15:40:15 -0400

    i think its good

--- hooks/snyk-precommit.sh
+++ hooks/snyk-precommit.sh
@@ -29,8 +29,7 @@ fi
 # https://support.snyk.io/hc/en-us/articles/360007487097-The-snyk-file
 function main {
     cd terraform/
-    SNKY_TEST=$(find . -name '*.tf' -not -path './.terraform/*' | xargs snyk iac test --severity-threshold=medium)
-    echo $SNKY_TEST
+    find . -name '*.tf' -not -path './.terraform/*' | xargs snyk iac test --severity-threshold=low
     local exit_code=$?
     return $exit_code
 }
